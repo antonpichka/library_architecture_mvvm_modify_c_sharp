3624e09cb959c154ff6b153e97d07ee35276ca45
antonpichka <antonpichka@gmail.com>
2024-03-11 17:39:01 +0200

    feature(Example): Add Example

---
+++ Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using library_architecture_mvvm_modify_c_sharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Utility.DebugPrint("Hello suka World");
+        Utility.DebugPrintException("Hello suka World");
+        Utility.DebugPrint("Hello suka World");
+        Utility.DebugPrint($"{EnumGuilty.Device}");
+    }
+}
